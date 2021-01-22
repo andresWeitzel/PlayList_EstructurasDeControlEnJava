7a2a8caf2fe46df7d6dd35bc3b37966646b6a781
andresWeitzel <andres96energy@hotmail.com>
2021-01-22 20:05:43 -0300

    Add files via upload

---
+++ CursoEstructuraControlJava/src/estructuracondicionalsimple/EstructuraCondicionalSimple.java
@@ -0,0 +1,27 @@
+
+package estructuracondicionalsimple;
+
+
+public class EstructuraCondicionalSimple {
+    
+    public static void main(String[] args) {
+        
+        int instruccion01;
+        int instruccion02;
+        int condicion=0;
+        
+        if(condicion== 0){
+            
+        int instruccion03;
+        int instruccion04;
+   
+        }else{
+            
+        int instruccion05;
+        int instruccion06;
+        
+        }
+       
+    }
+
+}
---
+++ CursoEstructuraControlJava/src/estructurasecuencial/EstructuraSecuencial.java
@@ -0,0 +1,19 @@
+
+package estructurasecuencial;
+
+
+public class EstructuraSecuencial {
+
+    
+    public static void main(String[] args) {
+        
+        int pepito=2;
+        
+        int juancito=5;
+        
+        System.out.println(pepito);
+        
+        System.out.println(juancito);
+        }
+
+}
